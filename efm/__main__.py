022b576e4620220283eeccf0f52dce9f58471fbb
Sean McCollum <anincrediblyshortname@gmail.com>
2025-03-05 02:28:14 -0800

    add watching

--- efm/__main__.py
+++ efm/__main__.py
@@ -2,7 +2,9 @@ import argparse
 import logging
 import os
 import textwrap
-
+import time
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 from efm.book import Book
 
 
@@ -59,29 +61,57 @@ def main():
 
     logging.basicConfig(level=loglevel)
 
-    books: list[Book] = []
     for root, dirs, files in os.walk(args.folder):
         for file in files:
             if file.endswith(".bak"):
                 continue
-            books.append(
+            process_book(
                 Book(
                     os.path.join(root, file), adobe_key_file=args.adobekey, dry=args.dry
-                )
+                ),
+                args.action,
             )
 
-    for book in books:
-        if "drm" in args.action:
-            book.remove_drm()
-        if "rename" in args.action:
-            book.rename()
-        if "print" in args.action:
-            book.print_metadata()
-        if "pdf" in args.action:
-            book.reformat_pdf()
-        if "none" in args.action:
-            book.get_metadata()
-        book.save()
+    if args.watch:
+        event_handler = OnBookChangeAdd(adobe_key_file=args.adobekey, dry=args.dry)
+        observer = Observer()
+        observer.schedule(event_handler, path=args.folder, recursive=True)
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        finally:
+            observer.stop()
+            observer.join()
+
+
+class OnBookChangeAdd(FileSystemEventHandler):
+    def __init__(self, adobe_key_file=str | None, dry=bool | None):
+        self.adobe_key_file = adobe_key_file
+        self.dry = dry
+
+    def on_created(self, event):
+        if event.is_file:
+            book = Book(event.src_path)
+            process_book(book, args.action)
+
+    def on_modified(self, event):
+        return super().on_modified(event)
+
+
+def process_book(book: Book, actions: list[str]):
+    # Note: this order has significance
+    if "drm" in actions:
+        book.remove_drm()
+    if "rename" in actions:
+        book.rename()
+    if "print" in actions:
+        book.print_metadata()
+    if "pdf" in actions:
+        book.reformat_pdf()
+    if "none" in actions:
+        book.get_metadata()
+    book.save()
 
 
 if __name__ == "__main__":
